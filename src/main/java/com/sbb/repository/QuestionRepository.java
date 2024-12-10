aa14e327f8ebe1ac30c687e41fa34c0234f769c7
kdo_1999/mac <kdo_1999@naver.com>
2024-12-10 12:18:27 +0900

    feat: QuestionRepository.findBySubjectLike 추가

--- src/main/java/com/sbb/repository/QuestionRepository.java
+++ src/main/java/com/sbb/repository/QuestionRepository.java
@@ -1,5 +1,7 @@
 package com.sbb.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sbb.entity.Question;
@@ -7,4 +9,5 @@ import com.sbb.entity.Question;
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 	Question findBySubject(String subject);
 	Question findBySubjectAndContent(String subject, String content);
+	List<Question> findBySubjectLike(String subject);
 }
