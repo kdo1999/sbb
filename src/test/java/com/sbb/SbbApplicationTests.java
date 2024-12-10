15ce0a43f7b576c2bd0b9f56fb8676160774c58a
kdo_1999/mac <kdo_1999@naver.com>
2024-12-10 12:01:17 +0900

    test: Question 저장 테스트

--- src/test/java/com/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbb/SbbApplicationTests.java
@@ -1,13 +1,36 @@
 package com.sbb;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sbb.entity.Question;
+import com.sbb.repository.QuestionRepository;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Transactional
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		Question q1 = new Question();
+		q1.setSubject("testSubject1");
+		q1.setContent("testContent1");
+		q1.setCreatedAt(LocalDateTime.now());
+
+		Question q2 = new Question();
+		q2.setSubject("testSubject2");
+		q2.setContent("testContent2");
+		q2.setCreatedAt(LocalDateTime.now());
+
+		questionRepository.save(q1);
+		questionRepository.save(q2);
 	}
 
 }
