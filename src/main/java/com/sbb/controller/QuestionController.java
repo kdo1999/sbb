5fb4ee4281419572f0a2945729e8f1f67e2d17c8
kdo_1999/mac <kdo_1999@naver.com>
2024-12-10 14:20:32 +0900

    feat: 타임리프 템플릿 적용

--- src/main/java/com/sbb/controller/QuestionController.java
+++ src/main/java/com/sbb/controller/QuestionController.java
@@ -1,14 +1,28 @@
 package com.sbb.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sbb.entity.Question;
+import com.sbb.repository.QuestionRepository;
+
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
-	@GetMapping("/question/list")
-	@ResponseBody
-	public String list() {
-		return "question list";
-	}
+
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
 }
