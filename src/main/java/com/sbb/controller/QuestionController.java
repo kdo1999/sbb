722bdf01ec85600da5c8471f194ed688d3662f32
kdo_1999/mac <kdo_1999@naver.com>
2024-12-10 14:10:46 +0900

    feat: QuestionController.list 추가

---
+++ src/main/java/com/sbb/controller/QuestionController.java
@@ -0,0 +1,14 @@
+package com.sbb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QuestionController {
+	@GetMapping("/question/list")
+	@ResponseBody
+	public String list() {
+		return "question list";
+	}
+}
