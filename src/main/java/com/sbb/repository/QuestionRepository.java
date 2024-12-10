bef6ff5a3d88cfe76e46a6ebdd5edd9b35d909b1
kdo_1999/mac <kdo_1999@naver.com>
2024-12-10 12:07:42 +0900

    feat: QuestionRepository.findBySubject 추가

--- src/main/java/com/sbb/repository/QuestionRepository.java
+++ src/main/java/com/sbb/repository/QuestionRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sbb.entity.Question;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+	Question findBySubject(String subject);
 }
