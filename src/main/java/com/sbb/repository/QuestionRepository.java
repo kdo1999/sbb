a9159c279ed3e1781ab74e95db7b416eccd3edfc
kdo_1999/mac <kdo_1999@naver.com>
2024-12-10 11:35:10 +0900

    feat: QuestionRepository 추가

---
+++ src/main/java/com/sbb/repository/QuestionRepository.java
@@ -0,0 +1,8 @@
+package com.sbb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sbb.entity.Question;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
